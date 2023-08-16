ad63b70202b56d3b19d596a8e58f4cda2c1dbdbd
satyamkumarjha9966 <satyamkumarjha9966@gmail.com>
2023-08-16 20:24:17 +0530

    React Page Routing Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import PolicyPage from "./pages/PolicyPage";
+import PageNotFoundPage from "./pages/PageNotFoundPage";
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>Ecommerce App</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/policy" element={<PolicyPage />} />
+        <Route path="*" element={<PageNotFoundPage />} />
+      </Routes>
     </>
   );
 }
